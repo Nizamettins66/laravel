99f6a42f08b9a9866506d2c0373147cbc451ff74
nizamettin sari <nizamettin.sari@student.rocva.nl>
2024-11-21 09:09:42 +0100

    Read

---
+++ stock/database/migrations/2024_11_14_101154_create_stocks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('stock_name');
+            $table->string('ticket');
+            $table->decimal('value', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stocks');
+    }
+};
---
+++ stock/resources/views/stocks/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Stocks</title>
+</head>
+<body>
+  <div class="container" style="margin:40px;">
+    <table class="table">
+      <thead class="thead-light">
+          <tr>
+            <th>ID</th>
+            <th>Stock Name</th>
+            <th>Stock Ticket</th>
+            <th>Stock Value</th>
+            <th>Updated at</th>
+          </tr>
+      </thead>
+      <tbody>
+          @foreach($stocks as $stock)
+          <tr>
+              <td>{{$stock->id}}</td>
+              <td>{{$stock->stock_name}} </td>
+              <td>{{$stock->ticket}}</td>
+              <td>{{$stock->value}}</td>
+              <td>{{$stock->updated_at}}</td>
+          </tr>
+          @endforeach
+      </tbody>
+    </table>
+  </div>
+
+</body>
+</html>
